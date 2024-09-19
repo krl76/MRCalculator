34e59bc10c6589d5fc482a7503973041bc846083
krl76 <k.kornilov2005@yandex.ru>
2024-09-19 16:36:38 +0300

    some changes

--- src/MRCalculatorCubes/Assets/Scripts/Calculator.cs
+++ src/MRCalculatorCubes/Assets/Scripts/Calculator.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Net;
 using TMPro;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Calculator : MonoBehaviour
